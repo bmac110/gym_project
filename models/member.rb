bd8194b9e15d602d5d26602ca8cb483dfdba4ee3
Billy Mackay <bossmack50@gmail.com>
2019-08-09 21:24:04 +0100

    passed multiple tests in member_spec

--- models/member.rb
+++ models/member.rb
@@ -2,18 +2,21 @@ require_relative("../db/sql_runner.rb")
 
 class Member
 
-  attr_accessor :name, :email, :phone
+  attr_accessor :first_name, :last_name, :email, :phone, :membership
   attr_reader :id
 
   def initialize(options)
     @id = options["id"].to_i if options["id"]
-    @name = options["name"]
+    @first_name = options["first_name"]
+    @last_name = options["last_name"]
     @email = options["email"]
     @phone = options["phone"].to_i()
+    @membership = options["membership"]
   end
 
   def save()
-    sql = "INSERT INTO members (name, email, phone) VALUES ($1, $2, $3) RETURNING id"
+    sql = "INSERT INTO members (first_name, last_name, email, phone, membership)
+    VALUES ($1, $2, $3, $4, $5) RETURNING id"
     values = [@name, @email, @phone]
     results = SqlRunner.run(sql, values)
     @id = results.first()["id"].to_i()
--- specs/member_spec.rb
+++ specs/member_spec.rb
@@ -6,14 +6,30 @@ class MemberTest < MiniTest::Test
 
   def setup()
     # @member = Member.new("Martin Porter", "martinp@net.com", 07445507653, "standard" )
-    options = {"id" => 1, "name" => "Martin Porter",
-      "email" => "martinp@net.com", "phone" => 07445507653, "membership" => "standard"}
+    options = {"id" => 1, "first_name" => "Ronald", "last_name" => "Macdonald",
+      "email" => "macgains@net.com", "phone" => 07445507653, "membership" => "standard"}
 
     @member = Member.new(options)
   end
 
-  def test_get_name()
-    assert_equal("Martin Porter", @member.name)
+  def test_get_first_name()
+    assert_equal("Ronald", @member.first_name)
+  end
+
+  def test_get_last_name()
+    assert_equal("Macdonald", @member.last_name)
+  end
+
+  def test_get_email()
+    assert_equal("macgains@net.com", @member.email)
+  end
+
+  def test_get_phone()
+    assert_equal(07445507653, @member.phone)
+  end
+
+  def test_get_membership()
+  assert_equal("standard", @member.membership)
   end
 
 end
