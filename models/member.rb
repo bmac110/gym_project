129e481825794a01594a9c1938f14c44ef038959
Billy Mackay <bossmack50@gmail.com>
2019-08-09 23:28:14 +0100

    added crud for member.rb

--- models/member.rb
+++ models/member.rb
@@ -22,4 +22,36 @@ class Member
     @id = results.first()["id"].to_i()
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM members"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM members"
+    results = SqlRunner.run(sql)
+    return results.map{|member| Member.new(member)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM members WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Member.new(results.first)
+  end
+
+  def update()
+    sql = "UPDATE members SET (first_name, last_name,
+    email, phone, membership)
+    = ($1, $2, $3, $4, $5) WHERE id = $6"
+    values = [@first_name, @last_name, @email, @phone, @membership, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM members WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
