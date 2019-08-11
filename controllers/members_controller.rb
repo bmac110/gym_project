6c551fa1e4bc77f7883dc1122b6864970dbc915c
Billy Mackay <bossmack50@gmail.com>
2019-08-11 15:59:37 +0100

    can now update member details

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -1,5 +1,3 @@
-require("sinatra")
-require("sinatra/contrib/all")
 require_relative("../models/member.rb")
 require_relative("../models/booking.rb")
 require_relative("../models/gym_class.rb")
@@ -15,18 +13,30 @@ get "/members/new" do
   erb (:"members/new")
 end
 
+
 get "/members/:id" do
   @member = Member.find(params["id"].to_i())
   erb (:"members/show")
 end
 
-post "/members/:id/delete" do
-  member = Member.find(params["id"])
-  member.delete
+get "/members/:id/edit" do
+  @member = Member.find(params["id"].to_i())
+  erb(:"members/edit")
+end
+
+post "/members/:id" do
+  member = Member.new(params)
+  member.update
   redirect to("/members")
 end
 
 post "/members" do
-  Member.new(params).save
+  Member.new(params).save()
+  redirect to("/members")
+end
+
+post "/members/:id/delete" do
+  member = Member.find(params["id"])
+  member.delete()
   redirect to("/members")
 end
